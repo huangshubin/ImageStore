8f83c8c797b2d0006e4a59a58eaa798bf9df0264
shubin <shubin@1211>
2016-06-23 09:22:31 +0800

    second

--- ImageWebAPIs/App_Start/WebApiConfig.cs
+++ ImageWebAPIs/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace ImageWebAPIs
@@ -20,7 +22,7 @@ namespace ImageWebAPIs
                 defaults: new { id = RouteParameter.Optional }
             );
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
-            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SerializerSettings.ContractResolver = new  CamelCasePropertyNamesContractResolver();
         }
     }
 }
--- ImageWebAPIs/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImageWebAPIs.WebApiApplication" Language="C#" %>
